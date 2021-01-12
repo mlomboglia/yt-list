14c5ab786a3b18cdf5e37929a26e2db68bf2bb19
Marcos Lomboglia <marcos.lomboglia@gmail.com>
2021-01-12 17:41:33 +0000

    Initial YoutubeAPI files and tests

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-// regex that checks if value contains digits only
-const digitsOnly = /^\d+$/;
+const axios = require("./youtube-axios");
+const youtubeAPI = require("./youtube-api");
 
-// function to mask digits into US phone format
-function maskUSPhone(phone) {
-  // returning null when getting null, undefined, or an object that is no string as a parameter
-  if (!phone || typeof phone !== 'string') {
-    return null;
-  }
-
-  // returning the untouched value when it contains non-digit chars
-  // or when it has a length lower than 10 or greater than 11
-  if (digitsOnly.test(phone) === false || phone.length < 10 || phone.length > 11) {
-    return phone;
-  }
-
-  // returning the masked value
-  const codeArea = phone.substring(0, 3); 
-  const prefix = phone.substring(3, 6);
-  const sufix = phone.substring(6, phone.length);
-  return `(${codeArea}) ${prefix}-${sufix}`;
-}
-
-export default maskUSPhone;
+exports.searchForVideos = async (searchQuery, nextPageToken, amount) => {
+  const config = youtubeAPI.buildSearchRequest(
+    searchQuery,
+    nextPageToken,
+    amount
+  );
+  return axios
+    .request(config)
+    .then((response) => {
+      const results = youtubeAPI.reduceSearchForVideos(
+        response.data,
+        searchQuery
+      );
+      return results;
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+};
